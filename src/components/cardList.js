60ed593219bbcbf7e8ce4fe45a6732eaca6b34e6
BuraIconic <buratesfa12@gmail.com>
2026-08-05 02:24:43 -0700

    width updating

--- src/components/cardList.js
+++ src/components/cardList.js
@@ -6,10 +6,10 @@ import '../container/index.css';
 const CardList=(props)=> {
  
   return (
-     <div className='bg-light-green dib br3 f6 fw6 ma2 grow  pa3   shadow-5 css cs'>
-        <img src= {`https:robohash.org/${props.id}`} alt="card" />
+     <div className='bg-light-green dib br3 f7 fw6 ma2 grow  pa3   shadow-5 css cs'>
+        <img src= {`https:robohash.org/${props.id}`} alt="card"  width="200px" height="200px" />
         <div className='desc'>
-           <h2>{props.name}</h2>
+           <h2 className="f5">{props.name}</h2>
            <h4>{props.email}</h4>
         </div>
     </div>
