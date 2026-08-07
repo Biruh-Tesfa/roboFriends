9373ff10aca86987648a7d909f2fbb2e3c05bbdb
BuraIconic <buratesfa12@gmail.com>
2026-08-07 00:41:34 -0700

    small edition

--- src/components/cardList.js
+++ src/components/cardList.js
@@ -7,7 +7,7 @@ const CardList=(props)=> {
  
   return (
      <div className='bg-light-green dib br3 f7 fw6 ma2 grow  pa3   shadow-5 css cs'>
-        <img src= {`https:robohash.org/${props.id}`} alt="card"  width="200px" height="200px" />
+        <img src= {`https://robohash.org/${props.id}`} alt="card"  width="200px" height="200px" />
         <div className='desc'>
            <h2 className="f5">{props.name}</h2>
            <h4>{props.email}</h4>
